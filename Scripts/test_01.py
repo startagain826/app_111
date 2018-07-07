c8f3fa56b9094c4a26d12981b3c485f8dbafb91b
start_again <cgc_020@163.com>
2018-07-07 13:39:51 +0800

    修改

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -1,7 +1,11 @@
-import allure
+import allure,pytest
 
 class Test_01:
 
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     @allure.step(title="第一个测试")
     def test_01_1(self):
+        allure.attach("这是一个描述", "我是具体内容")
+        allure.attach("这是二个描述", "我是内容")
+        allure.attach("这是三个描述", "。。。。。")
         assert 0
